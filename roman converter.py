55a867f76a918ff1aece2d31a4256c74a4b70796
philipebuka <philipebuka@gmail.com>
2022-12-03 06:11:06 +0100

    'added new commit':

---
+++ roman converter.py
@@ -0,0 +1,7 @@
+roman_figs = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+#num = input('Insert a number for conversion')
+#if num not in roman_figs.keys:
+ #   print(f'your input is not a letter in the roman figures, iput letters from {roman_figs}')
+#else: print(num)
+ls= [roman_figs.keys]
+ls
